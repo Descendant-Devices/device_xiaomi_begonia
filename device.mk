b2d400701d8c4a91437d1c59bae0e9b2270a2faf
TheMalachite <eliasgheeraert@gmail.com>
2022-07-27 01:26:52 +0200

    begonia: Drop MediaTek thermal services
    * mi_thermald do the job already

--- device.mk
+++ device.mk
@@ -364,14 +364,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtextclassifier_hash.vendor
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0.vendor \
-    android.hardware.thermal@2.0.vendor
-
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb.gadget@1.0.vendor \
